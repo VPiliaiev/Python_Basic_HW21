0188f253e489edd60ef7528e02178159d3d072d2
Valerii Piliaiev <133023422+VPiliaiev@users.noreply.github.com>
2024-03-07 14:39:25 +0200

    Created second version with costructor

---
+++ Python_Basic_HW21_OOP_Second_Version.py
@@ -0,0 +1,42 @@
+class Auto:
+    def __init__(self, brand, age, mark):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+        self.color = None
+        self.weight = None
+
+    def move(self):
+        print('move')
+
+    def birthday(self):
+        self.age += 1
+        print(f'Now the car is {self.age} years old')
+
+    def stop(self):
+        print('stop')
+
+
+car_1 = Auto('Toyota', 1999, 'Carola')
+car_1.color = 'red'
+
+car_1.move()
+car_1.birthday()
+car_1.stop()
+
+print(car_1.brand)
+print(car_1.age)
+print(car_1.color)
+print(car_1.mark)
+
+car_2 = Auto('Mercedes', 2002, 'C200')
+car_2.color = 'blue'
+
+print(car_2.brand)
+print(car_2.age)
+print(car_2.color)
+print(car_2.mark)
+
+car_2.move()
+car_2.birthday()
+car_2.stop()
